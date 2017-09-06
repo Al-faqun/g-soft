3135089d6b5e238999b75a05796f57447f26fda9
Alfa-Omega <inwronglocate@gmail.com>
2017-09-06 17:29:18 +0300

    Fixed bug in Request

--- src/Controllers/Request.php
+++ src/Controllers/Request.php
@@ -20,6 +20,10 @@ class Request
         $this->invert = $invert;
     }
     
+    /**
+     * Returns result of called callable, if all needed keys exists, or false.
+     * @return bool|mixed
+     */
     function call()
     {
         if (!$this->invert) {
@@ -47,10 +51,29 @@ class Request
             } else {
                 $return = false;
             }
+        //if we need to invert
         } else {
-            if ( !array_key_exists($this->key, $this->input) ) {
-                $return = ($this->callable)($this->key, null, $this->controller);
-            } else $return = false;
+            //if several keys
+            if (is_array($this->key)) {
+                //check that every key is present
+                $previousResult = true;
+                foreach ($this->key as $key) {
+                    //if every key exists -> proceed, else - do nothing
+                    if (!array_key_exists($key, $this->input) AND $previousResult === true) {
+                        $previousResult = true;
+                    } else {
+                        $previousResult = false;
+                    }
+                }
+                //if every key exists
+                if ($previousResult === true) {
+                    $return = ($this->callable)($this->key, null, $this->controller);
+                } else $return = false;
+                
+            //if single value key
+            } elseif (!array_key_exists($this->key, $this->input)) {
+                    $return = ($this->callable)($this->key, null, $this->controller);
+                } else $return = false;
         }
         
         return $return;
